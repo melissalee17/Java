a04e0fe06fecdb300304e4e001c2e93bb397002e
melissalee17 <melissaloreleilee@gmail.com>
2023-01-13 14:47:32 -0600

    Add files via upload

---
+++ Assignment 3/src/assignment/pkg3/Account.java
@@ -0,0 +1,28 @@
+package assignment.pkg3;
+
+public class Account
+{
+    protected String name; // The person who owns the account
+    protected double balance;// do not define this in CheckingAccount class
+ 
+    public Account(String acctName, double initBalance){
+             balance = initBalance;
+             name = acctName;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public double getBalance() {
+        return balance;
+    }
+ 
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
